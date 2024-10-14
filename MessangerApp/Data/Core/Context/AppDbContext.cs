fe67a769705c9c5bcacd1e93b2fee815012ed445
BeniLite <bysemenko@mail.ru>
2024-10-14 18:09:42 +0300

    3

---
+++ MessangerApp/Data/Core/Context/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Core.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(getSqliteConnect());
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        string getSqliteConnect()
+        {
+            var dir = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent;
+            return $@"Data source={dir}\Data\Core\Database\Messanger08.db";
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ MessangerApp/Data/Models/Message.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Message
+    {
+        public Message() { }
+
+        public Message(string text, User user, Chat chat)
+        {
+            Id = Guid.NewGuid();
+            Text = text;
+            SandDate = DateTime.Now;
+            User = user;
+            Chat = chat;
+            IsDeleted = false;
+        }
+
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid ChatId { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime SandDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public virtual User User { get; set; } = null!;
+        public virtual Chat Chat { get; set; } = null!;
+    }
+}
